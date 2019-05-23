c7b0a1a22be1b19193becb9b515d4e9d0b46d3ba
JeremyW1990 <shijie.wang1990@gmail.com>
2019-05-23 14:53:05 -0700

    write a javascript code under /util folder, creating a stream of consecutive geolocation information to stimulate a route. This file will be used to create dummy route data for testing purpose; create three models for tables

--- server/index.js
+++ server/index.js
@@ -2,15 +2,35 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const sequelize = require('../database/mysql.config');
+
+const User = require('./models/user');
+const Dog = require('./models/dog');
+
 const dogRoutes = require('./routes/dog');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api', dogRoutes);
 
-sequelize.sync()
+User.hasMany(Dog);
+Dog.belongsTo(User);
+User.create({ name: "Jeremy", email: 'cjwang1990@hotmail.com'})
+.then( res => {
+  console.log( "create user: ", res )
+})
+sequelize
+// .sync({ force: true })
+.sync()
   .then( result => {
-
+console.log("echo User", User);
+    return User.findById(1)
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: "Jeremy", email: 'cjwang1990@hotmail.com'})
+    }
+  })
+  .then( result => {
     app.listen(3001, () => {
       console.log("Server is listening at prot 3001");
     })
@@ -18,6 +38,5 @@ sequelize.sync()
   .catch( error => {
     console.log( error );
   })
-
 
 
