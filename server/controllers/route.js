c9f3f7840fe92b89756ce31b16e77fb513527674
JeremyW1990 <shijie.wang1990@gmail.com>
2019-05-24 10:31:01 -0700

    setup fetchAll controller; setup a get method , which leads to fetchall controller when /routes is entered; In app.js , setup the middleware for this routes;

---
+++ server/controllers/route.js
@@ -0,0 +1,17 @@
+const Route = require('../models/route');
+
+
+
+exports.getRoutes = (req, res, next) => {
+    console.log("Hit getRoutes");
+    Route.fetchAll()
+      .then( ([rows,fields]) => {
+        console.log(rows);
+        res.send(rows);
+      })
+      .catch( err => {
+          console.log(err);
+      });
+};
+
+
--- server/index.js
+++ server/index.js
@@ -1,42 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const sequelize = require('../database/mysql.config');
+const db = require('../database/mysql.config');
 
-const User = require('./models/user');
-const Dog = require('./models/dog');
-
-const dogRoutes = require('./routes/dog');
+const routeRoutes = require('./routes/route');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/api', dogRoutes);
 
-User.hasMany(Dog);
-Dog.belongsTo(User);
+app.use((req, res, next) => {
+  console.log('Universal Middleware test.');
+  next();
+});
 
 
-sequelize
-// .sync({ force: true })
-.sync()
-  .then( result => {
-    return User.findByPk(1)
-  })
-  .then(user => {
-    if (!user) {
-      return User.create({ name: "Jeremy", email: 'cjwang1990@hotmail.com'})
-    } 
-    else {
-      console.log('Find a user with ID 1')
-    }
-  })
-  .then( result => {
-    app.listen(3001, () => {
-      console.log("Server is listening at prot 3001");
-    })
-  })
-  .catch( error => {
-    console.log( error );
-  })
+app.use(routeRoutes);
 
+app.listen(3001, () => {
+  console.log("Server is listening at prot 3001");
+});
 
