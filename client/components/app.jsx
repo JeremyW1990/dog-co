bc1b0cde7781d8fa10b94dbca4305b3d6ac0546b
JeremyW1990 <shijie.wang1990@gmail.com>
2019-06-01 00:30:32 -0700

    add a root path to dog-co app

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -24,7 +24,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename='/dog-co'>
       <AuthContext.Provider value={{
           user_id: this.state.user_id,
           login: this.login
