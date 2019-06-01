febe4d83608af8ef51e2814ae745c2bf9d9bb006
JeremyW1990 <shijie.wang1990@gmail.com>
2019-06-01 00:14:39 -0700

    add public folder as middleware

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const db = require('../database/mysql.config');
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const routeRoutes = require('./routes/route');
 const geoLocationRoutes = require('./routes/geo-location');
